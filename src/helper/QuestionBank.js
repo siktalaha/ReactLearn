e64327346b0155c1c692b8f3eea90a3d3a83b3d4
Sikta Laha <siktalaha@gmail.com>
2023-03-05 02:10:05 +0530

    commiting the changes

--- src/helper/QuestionBank.js
+++ src/helper/QuestionBank.js
@@ -1,25 +1,53 @@
 export const Questions=[
     {
-    prompt:"What is project ?",
-    optionA:"VVgood",
-    optionB:"Thanks",
-    ans:"optionA"
+    prompt:"Environment is the life support system that includes?",
+    optionA:"Air",
+    optionB:"Water",
+    ans:"optionD"
+    },
+    {
+        prompt: "Which region gets first monsoon in summer?",
+        optionA:"Himalayas",
+        optionB:"Western Ghats",
+        ans:"optionB"
+    
+    },
+    {
+        prompt:"The organisms which feed on dead organisms and excreta of living organisms are known?",
+        optionA:"Decomposers",
+        optionB:"Consumers",
+        ans:"optionA"
+    
+    },
+    {
+        prompt:"Formation of hole in Ozone is maximum over ?",
+        optionA:" India",
+        optionB:"Antarctica",
+        ans:"optionB"
+    
+        },
 
-    },
-    {
-        prompt:"What is 2+1 ?",
-        optionA:"3",
-        optionB:"21",
-        ans:"optionA"
-    
-    },
-    {
-        prompt:"What is 2+11 ?",
-        optionA:"13",
-        optionB:"211",
-        ans:"optionA"
-    
-    },
-    
+        {
+            prompt:"In our country the percentage of land under forest is about: ?",
+            optionA:" 21.54%",
+            optionB:"30.24% ",
+            ans:"optionA"
+        
+            },
+            {
+                prompt:"Main source of acid rain is ?",
+                optionA:"Sulphur dioxide",
+                optionB:"Carbon dioxide",
+                ans:"optionA"
+            
+                },
+
+                {
+                    prompt:"Name the country which virtually closed cod fishery in the 1990’s due to depletion of fish reserves ?",
+                    optionA:"Canada",
+                    optionB:" Italy ",
+                    ans:"optionA"
+                
+                }    
 
 ]
